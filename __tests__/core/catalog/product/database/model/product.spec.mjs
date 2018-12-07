821553e6a0883e40b24d1bd0efe8b4bd7f9e5c94
Bruno Possidonio <bruno@usebeon.com>
2018-12-07 17:29:06 -0200

    add catalog/product/database/model spec

---
+++ __tests__/core/catalog/product/database/model/product.spec.mjs
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import Product from "../../../../../../src/core/catalog/product/database/model/product";
+
+describe(`product database model`, () => {
+    it(`should return a mongoose model`, () => {
+        const result = Product(mongoose);
+        expect(result).toHaveProperty("modelName", "Product");
+    });
+});
