88afa1e1e0bc40a085be2d109ba1453bcd3c8f80
Bruno Possidonio <bruno@usebeon.com>
2018-12-09 20:35:53 -0200

    add api/product implementation

---
+++ src/api/productApi.mjs
@@ -0,0 +1,3 @@
+export { default as getProduct } from "./product/getProduct";
+export { default as listProduct } from "./product/listProduct";
+export { default as saveProduct } from "./product/saveProduct";
