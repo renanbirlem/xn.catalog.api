27c241f8a858cd29148ee2b09caeeb6957f1d7ac
Bruno Possidonio <bruno@usebeon.com>
2018-12-09 22:13:59 -0200

    feat(api/product/save): implement method

--- src/api/product/saveProduct.mjs
+++ src/api/product/saveProduct.mjs
@@ -1,8 +1,34 @@
-export default ({ client_id, key = "sku", ...search }) =>
-    new Promise((resolve, reject) => {
+import saveProductToDatabase from "../../core/product/database/saveProductToDatabase";
+import saveProductToCache from "../../core/product/cache/saveProductToCache";
+
+export default ({ client_id, sku, document }) =>
+    new Promise(async (resolve, reject) => {
         try {
-            resolve();
+            if (!client_id) {
+                throw new Error(`client_id must be informed.`);
+            }
+
+            if (!sku) {
+                throw new Error(`sku must be informed.`);
+            }
+
+            if (!document) {
+                throw new Error(`document must be informed.`);
+            }
+
+            const result = await saveProductToDatabase({
+                client_id,
+                sku,
+                document
+            });
+
+            if (result) {
+                saveProductToCache({ client_id, sku, product: result });
+                resolve(result);
+            } else {
+                resolve(null);
+            }
         } catch (e) {
-            reject(new Error(`Unknown error ${e.message}`));
+            reject(e);
         }
     });
