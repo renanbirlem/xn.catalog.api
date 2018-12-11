0f97639295b0b2f50a56cf28c3ec2d76ea83cf75
Bruno Possidonio <bruno@usebeon.com>
2018-12-11 14:04:47 -0200

    feat(rest/product): implement post method

--- src/rest/product.mjs
+++ src/rest/product.mjs
@@ -1,10 +1,20 @@
 import express from "express";
 import debug from "debug";
+import productApi from "../api/productApi";
 
 const log = debug("app:rest:product");
 
 const api = express.Router({ mergeParams: true });
 
 api.get("/", (request, response) => response.sendStatus(501));
+
+api.post("/:sku", (request, response) => {
+    const { client_id, sku } = request.params;
+    const document = request.body;
+
+    const result = productApi.saveProduct({ client_id, sku, document });
+    result.then(result => response.json(result));
+    result.catch(error => response.sendStatus(500, error));
+});
 
 export default api;
